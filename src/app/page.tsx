29557b4300457f1d8ef7ba0cb7c8325bd59c66f8
Aditya Shukla <Shadowshukla76@gmail.com>
2025-08-06 23:46:05 +0530

    Added Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import { DemoBlogs } from "@/components/global/hero/DemoBlogs";
 import Features from "@/components/global/hero/Features";
 import Footer from "@/components/global/hero/Footer";
 import { HeroSection } from "@/components/global/hero/Hero";
+import Navbar from "@/components/global/hero/Navbar";
 import Testimonials from "@/components/global/hero/Testimonials";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center">
+      <Navbar/>
       <HeroSection />
       <Features />
       <Testimonials />
---
+++ src/components/global/hero/Navbar.tsx
@@ -0,0 +1,77 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="bg-white w-full">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="md:flex md:items-center md:gap-12">
+            <Link className="block text-black cursor-pointer" href="#">
+              <span className="text-black text-xl underline font-bold">
+                Notefolio
+              </span>
+            </Link>
+          </div>
+
+          <div className="hidden md:block">
+            <nav aria-label="Global">
+              <ul className="flex items-center gap-6 text-sm">
+                <li>
+                  <Link
+                    className="text-gray-800 hover:text-gray-700 hover:underline duration-300 transition-all"
+                    href="/blogs"
+                  >
+                    {" "}
+                    Blogs
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <div className="sm:flex sm:gap-4">
+              <Link
+                className=" bg-slate-50 hover:bg-slate-100 px-8  py-2.5 text-sm font-medium text-black shadow-sm rounded-full transition-all duration-500"
+                href="/auth/login"
+              >
+                Login
+              </Link>
+
+              <div className="hidden sm:flex">
+                <Link
+                  className=" bg-stone-800 hover:bg-stone-900 transition-all duration-500 px-8  py-2.5 text-sm font-medium text-white shadow-sm rounded-full"
+                  href="/auth/register"
+                >
+                  Register
+                </Link>
+              </div>
+            </div>
+
+            <div className="block md:hidden">
+              <button className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
