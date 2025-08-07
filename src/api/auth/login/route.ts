145eafd638234f1655f407c56a60c6904500e8b7
Aditya Shukla <Shadowshukla76@gmail.com>
2025-08-07 08:56:58 +0530

    added login and register user

---
+++ src/api/auth/login/route.ts
@@ -0,0 +1,64 @@
+import { db } from "@/db/db";
+import { User } from "@/schemas/User";
+import bcrypt from "bcrypt";
+import { eq } from "drizzle-orm";
+import jwt from "jsonwebtoken";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { email, password } = body;
+  if (!email || !password) {
+    return new Response(JSON.stringify({ success: false }), {
+      status: 400,
+      statusText: `Necessary Details are not provided`,
+    });
+  }
+  const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+  if (!JWT_SECRET_KEY) {
+    return new Response(JSON.stringify({ success: false }), {
+      status: 500,
+      statusText: `Server Error`,
+    });
+  }
+  try {
+    const users = await db.select().from(User).where(eq(User.email, email));
+    const user = users[0];
+
+    if (!user) {
+      return new Response(JSON.stringify({ success: false }), {
+        status: 404,
+        statusText: `User with email: ${email} not found`,
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return new Response(
+        JSON.stringify({ success: false, message: "Invalid password" }),
+        {
+          status: 401,
+        }
+      );
+    }
+    const jwtToken = jwt.sign(
+      { id: user.id, email: user.email },
+      JWT_SECRET_KEY,
+      {
+        algorithm: "HS256",
+        expiresIn: "7d",
+      }
+    );
+    return new Response(
+      JSON.stringify({ success: true, jwtToken, message: `Login Successfull` }),
+      {
+        status: 200,
+      }
+    );
+  } catch (error: unknown) {
+    console.error(`Failed to Login User ${error}`);
+    return new Response(JSON.stringify({ success: false, error }), {
+      status: 500,
+      statusText: `Failed To Login User. Server Error`,
+    });
+  }
+}
--- src/api/auth/register/route.ts
+++ src/api/auth/register/route.ts
@@ -1,4 +1,40 @@
-export async function POST(request: Request) {
+import { db } from "@/db/db";
+import { User } from "@/schemas/User";
+import bcrypt from "bcrypt";
+
+export async function POST(request: Request, res: Response) {
   const body = await request.json();
-  const {username,email,password,profileUrl,bio}=body
+  const { username, email, password, profileUrl, bio } = body;
+  if (!username || !email || !password) {
+    return new Response(JSON.stringify({ success: false }), {
+      status: 400,
+      statusText: `Necessary Details are not provided`,
+    });
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = {
+      id: crypto.randomUUID(),
+      username,
+      email,
+      password: hashedPassword,
+      profileUrl,
+      bio,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    await db.insert(User).values({ ...user });
+    return new Response(
+      JSON.stringify({ success: true, message: `User Created Successfully` }),
+      {
+        status: 201,
+      }
+    );
+  } catch (error: unknown) {
+    console.error(`Failed to Create User ${error}`);
+    return new Response(JSON.stringify({ success: false, error }), {
+      status: 500,
+      statusText: `Failed To Create User. Server Error`,
+    });
+  }
 }
