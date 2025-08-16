ae1e9375837c4caefa265e9512f5252ff05aad24
Aditya Shukla <Shadowshukla76@gmail.com>
2025-08-16 10:43:03 +0530

    added tags as a column in blogs

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -2,7 +2,7 @@ import { db } from "@/db/db";
 import { BlogSchema } from "@/schemas/Blog";
 import { tagTable } from "@/schemas/Tag";
 import { verifyUser } from "@/services/VerifyUser";
-import { ilike, or, sql } from "drizzle-orm";
+import { eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
 import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
@@ -48,13 +48,13 @@ export async function POST(request: Request) {
       await db.insert(tagTable).values(tagValues);
     }
     return NextResponse.json(
-      { message: "Blog created successfully", blog: newBlog,success:true },
+      { message: "Blog created successfully", blog: newBlog, success: true },
       { status: 201 }
     );
   } catch (error) {
     console.error("Error creating blog:", error);
     return NextResponse.json(
-      { error: "Failed to Create Blog "+error },
+      { error: "Failed to Create Blog " + error },
       { status: 500 }
     );
   }
@@ -71,34 +71,47 @@ export async function GET(request: Request) {
     const whereCondition = search
       ? or(
           ilike(BlogSchema.blogTitle, `%${search}%`),
-          sql`EXISTS (
-    SELECT 1 FROM ${tagTable}
-    WHERE ${tagTable.blogId} = ${BlogSchema.id}
-    AND ${tagTable.tag} ILIKE ${`%${search}%`}
-  )`
+          sql`
+      EXISTS(
+        SELECT 1 FROM ${tagTable}
+        WHERE ${tagTable.blogId}=${BlogSchema.id}
+        AND ${tagTable.tag} ilike ${`%${search}%`}
+      ) `
         )
       : undefined;
+
     const blogs = await db
-      .select()
+      .select({
+        ...getTableColumns(BlogSchema),
+        tags: sql<
+          string[]
+        >`coalesce(array_agg(${tagTable.tag}),'{}'::text[])`.as("tags"),
+      })
       .from(BlogSchema)
+      .leftJoin(tagTable, eq(BlogSchema.id, tagTable.blogId))
+      .groupBy(BlogSchema.id)
       .where(whereCondition || sql`TRUE`)
       .limit(limit)
       .offset(offset);
+
     const [{ count }] = await db
       .select({ count: sql<number>`COUNT(*)` })
       .from(BlogSchema)
       .where(whereCondition || sql`TRUE`);
+
     return new Response(
       JSON.stringify({
         data: blogs,
         pagination: {
           total: Number(count),
-          page,
           limit,
+          page,
           totalPages: Math.ceil(Number(count) / limit),
         },
       }),
-      { status: 200 }
+      {
+        status: 200,
+      }
     );
   } catch (error) {
     console.error(`Failed to get all blogs `, error);
