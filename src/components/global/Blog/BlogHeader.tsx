52fce6040082f57f6e8567e2e204b9bdb82f4373
Aditya Shukla <Shadowshukla76@gmail.com>
2025-08-18 08:39:15 +0530

    added delete and edit icon

--- src/components/global/Blog/BlogHeader.tsx
+++ src/components/global/Blog/BlogHeader.tsx
@@ -6,6 +6,18 @@ import { format } from "date-fns";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/context/AuthContext";
 import { toast } from "sonner";
+import { EditIcon, Trash } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 interface BlogHeaderProps {
   blogTitle: string;
@@ -64,6 +76,35 @@ const BlogHeader = ({
             </p>
           </div>
         </div>
+
+        {!loading && isAuthenticated && user && authorId === user.id && (
+          <div className="flex items-center space-x-4">
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <div className="flex items-center justify-center cursor-pointer bg-red-500 hover:bg-red-600 rounded-full h-10 w-10">
+                  <Trash size={20} className="text-xs  text-white " />
+                </div>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your blog and remove your data from our servers.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction>Yes, Delete</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+
+            <div className="flex items-center justify-center cursor-pointer bg-green-500 hover:bg-green-600 rounded-full h-10 w-10">
+              <EditIcon size={20} className="text-xs  text-white " />
+            </div>
+          </div>
+        )}
 
         {!loading && authorId !== user?.id && (
           <Button
