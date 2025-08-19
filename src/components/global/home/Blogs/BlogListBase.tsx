5dc7e172ef2e3843b3f51c8811c4a963b170cd7b
Aditya Shukla <Shadowshukla76@gmail.com>
2025-08-19 07:11:48 +0530

    quick fix

--- src/components/global/home/Blogs/BlogListBase.tsx
+++ src/components/global/home/Blogs/BlogListBase.tsx
@@ -4,9 +4,8 @@ import React from "react";
 import BlogCard from "../../Blog/BlogCard";
 import { SingleBlogInterface } from "@/types/Blog/Blog";
 const BlogListBase = ({ data }: { data: SingleBlogInterface[] }) => {
-    console.log("data ",data)
   return (
-    <section className="w-full flex-1 h-full py-4 max-w-7xl grid  md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8">
+    <section className="w-full flex-1 h-full py-4 max-w-7xl grid  md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8  px-5">
       {data &&
         data?.length > 0 &&
         data.map((d, i) => <BlogCard blog={d} key={i} />)}
