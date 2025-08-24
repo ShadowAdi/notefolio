7110e91fc6e971f26f39175bcbce328888a9cc1f
Aditya Shukla <Shadowshukla76@gmail.com>
2025-08-24 20:25:29 +0530

    added dropdown

--- src/components/global/home/HomeNavbar.tsx
+++ src/components/global/home/HomeNavbar.tsx
@@ -1,19 +1,27 @@
 "use client";
 import { useAuth } from "@/context/AuthContext";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import PublishModal from "./write/PublishModal";
 import PublishEditModal from "../Blog/Edit/PublishEditModal";
-import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const HomeNavbar = () => {
-  const { user, isAuthenticated, loading } = useAuth();
+  const { user, isAuthenticated, loading, logout } = useAuth();
   const pathname = usePathname();
+  const router = useRouter();
 
   return (
     <header className="bg-white py-3 w-full">
-      <div className="mx-auto max-w-screen-xl flex flex-row items-center justify-between px-4 sm:px-6 lg:px-8 py-2">
+      <div className="mx-auto max-w-[90%] flex flex-row items-center justify-between px-4 sm:px-6 lg:px-8 py-2">
         <div className="flex items-center justify-between">
           <div className="md:flex md:items-center md:gap-12">
             <Link className="block text-black cursor-pointer" href="/">
@@ -40,22 +48,45 @@ const HomeNavbar = () => {
             ? "Loading..."
             : isAuthenticated &&
               user && (
-                <Link
-                  href={"/profile"}
-                  className="relative w-10 h-10 rounded-full overflow-hidden"
-                >
-                  {user.profileUrl ? (
-                    <img
-                      src={user.profileUrl}
-                      alt={user.profileUrl}
-                      className="object-cover"
-                    />
-                  ) : (
-                    <div className="w-full h-full bg-gray-300 flex items-center justify-center text-gray-600 text-lg">
-                      {user.username ? user.username[0].toUpperCase() : "U"}
-                    </div>
-                  )}
-                </Link>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Link
+                      href={"/profile"}
+                      className="relative w-10 h-10 rounded-full overflow-hidden"
+                    >
+                      {user.profileUrl ? (
+                        <img
+                          src={user.profileUrl}
+                          alt={user.profileUrl}
+                          className="object-cover"
+                        />
+                      ) : (
+                        <div className="w-full h-full bg-gray-300 flex items-center justify-center text-gray-600 text-lg">
+                          {user.username ? user.username[0].toUpperCase() : "U"}
+                        </div>
+                      )}
+                    </Link>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel
+                      className="cursor-pointer hover:bg-gray-100"
+                      onClick={() => {
+                        router.push("/profile");
+                      }}
+                    >
+                      My Account
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem
+                      className="cursor-pointer"
+                      onClick={() => {
+                        logout();
+                      }}
+                    >
+                      Logout
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
               )}
         </div>
       </div>
