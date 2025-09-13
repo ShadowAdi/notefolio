6bcceb5ebaf50cc97069aa55d1a518a5ad4260f1
Aditya Shukla <Shadowshukla76@gmail.com>
2025-09-13 10:04:03 +0530

    added get requedt

--- src/app/api/blog/draft/route.ts
+++ src/app/api/blog/draft/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/db/db";
 import { BlogSchema } from "@/schemas/Blog";
 import { verifyUser } from "@/services/VerifyUser";
+import { eq } from "drizzle-orm";
 import { v4 as uuidv4 } from "uuid";
 
 export async function POST(request: Request) {
@@ -59,3 +60,43 @@ export async function POST(request: Request) {
     );
   }
 }
+
+
+export async function GET(request: Request) {
+  try {
+    const user = await verifyUser(request);
+    if (!user) {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: `You are not Authorized`,
+        }),
+        { status: 401 }
+      );
+    }
+    const blogs = await db
+      .select()
+      .from(BlogSchema)
+      .where(eq(BlogSchema.authorId,user.id))
+
+    return new Response(
+      JSON.stringify({
+        success: true,
+        blogId: blogs,
+      }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(`Error in getting draft blogs: ${error}`);
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error,
+      }),
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
